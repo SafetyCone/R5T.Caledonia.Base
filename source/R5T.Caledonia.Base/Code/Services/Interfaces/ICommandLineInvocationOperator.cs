3351bead17f232f1b9f117e577af9bb95026770c
MinexAutomation <minex.automation@gmail.com>
2019-10-29 14:08:11 -0700

    Added command-line invocation operator service interface.

---
+++ source/R5T.Caledonia.Base/Code/Services/Interfaces/ICommandLineInvocationOperator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace R5T.Caledonia
+{
+    public interface ICommandLineInvocationOperator
+    {
+        int Run(string command, string arguments, DataReceivedEventHandler receiveOutputData, DataReceivedEventHandler receiveErrorData);
+    }
+}
