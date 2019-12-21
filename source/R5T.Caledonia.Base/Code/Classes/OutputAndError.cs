d7d1df76a6e88d56a1b68f7c145d245244562e32
MinexAutomation <minex.automation@gmail.com>
2019-12-21 13:08:30 -0800

    Added OutputAndError class type.

---
+++ source/R5T.Caledonia.Base/Code/Classes/OutputAndError.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Caledonia
+{
+    public class OutputAndError
+    {
+        public string Output { get; set; }
+        public string Error { get; set; }
+    }
+}
