2473d58c24ae04c3c681fc65958feaaefeec9cfb
davidcoats <david@getsafetycone.com>
2022-04-27 15:25:53 -0700

    Interim changes.

---
+++ source/R5T.Caledonia.Base/Code/Services/Definitions/ICommandLineInvocationOperator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+using R5T.T0064;
+
+
+namespace R5T.Caledonia
+{
+    [ServiceDefinitionMarker]
+    public interface ICommandLineInvocationOperator : IServiceDefinition
+    {
+        int Run(string command, string arguments, DataReceivedEventHandler receiveOutputData, DataReceivedEventHandler receiveErrorData);
+
+        CommandLineInvocationResult Run(CommandLineInvocation invocation);
+    }
+}
